307babdf8a17ddb0c73dc96338b9e8863beead81
Amandell <amanda.mandell2@gmail.com>
2022-01-31 18:47:21 -0500

    added javascript planning comments

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
         var secondsLabel = document.getElementById("seconds");
         var totalSeconds = 0;
 
+        //intializes the timer
         function setTime()
         {   
             ++totalSeconds;
@@ -10,6 +11,7 @@
             
         }
 
+        //makes timer run
         function pad(val)
         {
             var valString = val + "";
@@ -23,14 +25,40 @@
             }
         }
         
+        //initially shuffle cards
+
+        //begins the timer on start click
         const start = document.getElementById("start");
         start.addEventListener("click",() => {
             intervalID = setInterval(setTime, 1000);
         })
 
+        //resets the timer on reset click
+        /*NEED TO ADD PAUSE TO TIMER ON RESET
+        NEED TO SHUFFLE CARDS WHEN RESET IS CLICKED
+        */
         const reset = document.getElementById("reset");
         reset.addEventListener("click",() => {
             totalSeconds = 0;
             document.getElementById("minute").innerHTML = '0';
             document.getElementById("seconds").innerHTML = '0';
         })
+
+        //function that stops timer when last two cards match
+
+        //function that shuffles on start and reset
+
+        //function that flips the card to show images
+
+        //create a function that lets us only click two at a time (locks gameboard)
+
+        //first card should stay flipped until second card is flipped
+
+        //function to determine matches or not
+
+
+        /*EXTRAS
+        -something that logs the number of matches
+        -congrats screen at the end with time logged
+        -add flipping animation
+         */
